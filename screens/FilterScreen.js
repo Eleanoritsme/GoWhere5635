6d12aa5005a787c01a5caab1d927390aeb1b41df
Callieeechen <e0959468@u.nus.edu>
2023-06-02 22:01:19 +0800

    package changes

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const FilterScreen = () => {
   return (
-    <View>
-      <Text>FilterScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.title}>
+        <Text>Select Your Preferences</Text>
+      </View>
+    </SafeAreaView>
+
   )
 }
 
 export default FilterScreen
+
+const styles = StyleSheet.create({
+  title:{
+    marginLeft:20,
+    marginTop:20,
+    marginBottom:40
+  },
+})
