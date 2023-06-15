00b018ce016e11b35a24bd77b5ce29633c7e3896
Callieeechen <e0959468@u.nus.edu>
2023-06-15 14:01:51 +0800

    fetch data changes

--- screens/RecommandationScreen.js
+++ screens/RecommandationScreen.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const RecommandationScreen = () => {
+const RecommandationScreen = ({route}) => {
+  const {recommendations} = route.params;
+
   return (
-    <View>
-      <Text>RecommandationScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          {recommendations && recommendations.businesses && recommendations.businesses.map((item, index) => (
+            <View key={index} style={{ marginBottom: 20 }}>
+              <Image source={{ uri: item.image_url }} style={{ width: 100, height: 100 }} />
+              <Text>{item.name}</Text>
+              <Text>{item.location.address1}</Text>
+              {/* Add more fields as needed */}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
 
 export default RecommandationScreen
