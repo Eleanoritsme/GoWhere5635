e4f0b4adf3e489c3389c7ad1bd0daaa8df77aec7
Eleanor Zheng <122248712+Eleanoritsme@users.noreply.github.com>
2023-06-21 21:14:56 +0800

    Visited Places List Page (Places) UI setup
    completed

---
+++ screens/VisitedPlaceListScreen.js
@@ -0,0 +1,78 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useCallback } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { firebase } from '../config'
+import { useNavigation } from '@react-navigation/native'
+import { ScrollView } from 'react-native-gesture-handler'
+
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+const VisitedPlaceListScreen = () => {
+  const navigation = useNavigation()
+  const [fontsLoaded] = useFonts({
+    "Inter-SemiBold": require('../assets/fonts/Inter-SemiBold.ttf'),
+    "Inter-ExtraBold": require('../assets/fonts/Inter-ExtraBold.ttf'),
+    "Inter-Bold": require('../assets/fonts/Inter-Bold.ttf'),
+    "Inter-Medium": require('../assets/fonts/Inter-Medium.ttf'),
+    "Inter-Regular": require('../assets/fonts/Inter-Regular.ttf')
+  });
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ScrollView
+      style={{flex:1}}
+      contentContainerStyle={{alignContent:'flex-start', paddingBottom:60}}
+      showsVerticalScrollIndicator={false}
+      onLayout={onLayoutRootView}
+    >
+    <View style={{
+      height:'100%',
+      alignItems:'center',
+      justifyContent:'center'
+    }}>
+      <Text style={{
+        textAlign:'center',
+        fontFamily:'Inter-Regular',
+        color:'#949494',
+        marginTop:20,
+        fontSize:14,
+      }}>You have not been to any places yet.</Text>
+      <Text style={{
+        color:'#949494',
+        fontFamily:'Inter-Regular',
+        fontSize:14,
+        marginTop:10,
+      }}>Go to find some good recommendations and have a try!</Text>
+      <TouchableOpacity onPress={() => {navigation.navigate('Activity')}}
+        style={{
+          marginTop:10,
+          borderRadius:15,
+          width:80,
+          height:30,
+          alignItems:'center',
+          justifyContent:'center',
+          borderWidth:1,
+          borderColor:'#949494',
+        }}>
+        <Text>
+        Have a try
+        </Text>
+      </TouchableOpacity>
+    </View>
+    
+    
+    </ScrollView>
+  )
+}
+
+export default VisitedPlaceListScreen
