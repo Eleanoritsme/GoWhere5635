d05b96f453a85c1b0269badccb1b464193d83caa
Callieeechen <e0959468@u.nus.edu>
2023-06-30 15:18:22 +0800

    changes

---
+++ Line.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Line() {
+    return (
+      <View
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: '#CBC4C4',
+        width: 327,
+        marginBottom:20,
+      }}
+    />
+    )
+}
